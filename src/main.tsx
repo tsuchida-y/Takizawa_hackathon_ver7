9ece9235b7f862d5efd7df65e38153d6b0a76226
tatsuki-s <zekoushn@gmail.com>
2025-03-16 01:06:19 +0900

    認証画面だけ作成

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './pages/App.tsx';
+import Auth from './pages/auth.tsx';
 import BusinessDashboard from './pages/BusinessDashboard.tsx';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
+      <Route path="/auth" element={<Auth />}/>
       <Route path="/home" element={<App />} />
       <Route path="/businessdashboard" element={<BusinessDashboard />} />
     </Routes>
---
+++ src/pages/Auth.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Auth = () => {
+  return (
+    <div>
+      <p>これは子コンポーネントです。</p>
+    </div>
+  );
+};
+
+export default Auth;
